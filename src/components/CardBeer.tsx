393667107732613064f7c7fe647f4b97eccf6b9f
Sorachat <73382296+ter2002@users.noreply.github.com>
2022-12-22 03:25:28 +0700

    Fix interface beer

--- src/components/CardBeer.tsx
+++ src/components/CardBeer.tsx
@@ -1,63 +1,80 @@
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import IconButton from '@mui/material/IconButton';
-import { Link } from 'react-router-dom';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import IconButton from "@mui/material/IconButton";
+import { Link } from "react-router-dom";
 interface BeerProps {
-    id: number;
-    name: string;
-    description: string;
-    imageUrl: string;
-    isLogin: boolean;
+  _id: string;
+  name: string;
+  description: string;
+
+  imageUrl: string;
+  userId: string;
+  isLogin: boolean;
 }
 
-export default function CardBeer({ id, name, description, imageUrl, isLogin }: BeerProps) {
+export default function CardBeer({
+  _id,
+  name,
+  description,
+  imageUrl,
+  isLogin,
+}: BeerProps) {
+  const MAX_LENGTH_DESCRIPTION = 30;
 
+  if (description.length > MAX_LENGTH_DESCRIPTION) {
+    description = description.slice(0, MAX_LENGTH_DESCRIPTION) + "...";
+  }
 
-    const MAX_LENGTH_DESCRIPTION = 30
+  const addToFavoriteBeer = (id: string) => {
+    console.log("add");
+  };
 
-    if (description.length > MAX_LENGTH_DESCRIPTION) {
-        description = description.slice(0, MAX_LENGTH_DESCRIPTION) + "...";
-    }
-
-    const addToFavoriteBeer = (id: string) => {
-        console.log("add")
-    }
-
-    return (
-        <Card sx={{ maxWidth: 345 }} className="center-y" >
-            <CardMedia
-                component="img"
-                height="140"
-                image={imageUrl ? imageUrl : "https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="}
-                alt="green iguana"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" className='word-wrap'>
-                    {name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary" className='word-wrap'>
-                    {description}
-                </Typography>
-            </CardContent>
-            <CardActions >
-                {(isLogin) && (
-                    <IconButton aria-label="add to favorites" onClick={() => addToFavoriteBeer("3")}>
-                        <FavoriteIcon />
-                    </IconButton>
-                )}
-                {id != 0 ? (
-                    <Link style={{ textDecoration: 'none' }} to={`/beer/${id}`}>
-                        <Button size="small">อ่านเพิ่มเติม</Button>
-                    </Link>
-                ) : (
-                    <Button size="small">อ่านเพิ่มเติม</Button>
-                )}
-            </CardActions>
-        </Card>
-    );
+  return (
+    <Card sx={{ maxWidth: 345 }} className="center-y">
+      <CardMedia
+        component="img"
+        height="140"
+        image={
+          imageUrl
+            ? imageUrl
+            : "https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="
+        }
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" className="word-wrap">
+          {name}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          className="word-wrap"
+        >
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {isLogin && (
+          <IconButton
+            aria-label="add to favorites"
+            onClick={() => addToFavoriteBeer("3")}
+          >
+            <FavoriteIcon />
+          </IconButton>
+        )}
+        {_id !== "0" ? (
+          <Link style={{ textDecoration: "none" }} to={`/beer/${_id}`}>
+            <Button size="small">อ่านเพิ่มเติม</Button>
+          </Link>
+        ) : (
+          <Button size="small">อ่านเพิ่มเติม</Button>
+        )}
+      </CardActions>
+    </Card>
+  );
 }
--- src/screens/BeerDetailScreen.tsx
+++ src/screens/BeerDetailScreen.tsx
@@ -1,126 +1,142 @@
-import Paper from '@mui/material/Paper';
-import Grid from "@mui/material/Grid"
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import { useState } from 'react';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { useState, useEffect } from "react";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import axios from "axios";
 
 interface Ingredient {
-    name: string;
-    quantity: number;
-    unit: string;
+  name: string;
+  quantity: number;
+  unit: string;
 }
 
 interface Beer {
-    id: number;
-    name: string;
-    description: string;
-    ingredients: Ingredient[];
-    methods: string[];
+  id: number;
+  name: string;
+  description: string;
+  ingredients: Ingredient[];
+  methods: string[];
 }
 
 interface IsLoginProp {
-    isLogin: boolean;
+  isLogin: boolean;
 }
 
 export default function BeerDetailScreen({ isLogin }: IsLoginProp) {
+  const [beer, setBeer] = useState<Beer>({
+    id: 4,
+    name: "เบียร์แมว",
+    description: "เบียร์ที่ทำจากแมวช็อคโกแลต",
+    ingredients: [
+      {
+        name: "ช็อคโกแลต",
+        quantity: 3,
+        unit: "แท่ง",
+      },
+      {
+        name: "หม้อ",
+        quantity: 1,
+        unit: "อัน",
+      },
+    ],
+    methods: ["เอาช็อคโกแลตไปไก่", "เอาไก่ไปต้ม", "พร้อมรับประทาน"],
+  });
+//   useEffect(() => {
+//     async function fetchBeers() {
+//       try {
+//         axios.get("http://localhost:8080/beers/").then((response) => {
+//           setBeer(response.data);
+//           console.log(
+//             "🚀 ~ file: HomeScreen.tsx:46 ~ axios.get ~ response",
+//             response
+//           );
+//         });
+//       } catch (e) {
+//         console.log(e);
+//       }
+//     }
+//     fetchBeers();
+//   }, []);
 
-    const [beer, setBeer] = useState<Beer>({
-        id: 4,
-        name: "เบียร์แมว",
-        description: "เบียร์ที่ทำจากแมวช็อคโกแลต",
-        ingredients: [{
-            name: "ช็อคโกแลต",
-            quantity: 3,
-            unit: "แท่ง"
-        }, {
-            name: "หม้อ",
-            quantity: 1,
-            unit: "อัน"
-        }],
-        methods: ['เอาช็อคโกแลตไปไก่', 'เอาไก่ไปต้ม', 'พร้อมรับประทาน']
-    });
-
-
-    return (
-        <>
-            <Container maxWidth="sm" className="p-16">
-                <Grid container spacing={2}>
-                    <Grid item xs={12} style={{ position: 'relative' }}>
-                        <img src="https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w=" className='cover-image' alt="รูปภาพเบียร์"></img>
-                        <Typography variant="h3" className='center-offset'>
-                            {beer.name}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Paper className="p-16" >
-                            <Typography variant="h5">
-                                รายละเอียด
-                            </Typography>
-                            <Paper className="p-16" elevation={4} style={{ minHeight: "50px" }}>
-                                <Typography variant="subtitle2">
-                                    {beer.description}
-                                </Typography>
-                            </Paper>
-
+  return (
+    <>
+      <Container maxWidth="sm" className="p-16">
+        <Grid container spacing={2}>
+          <Grid item xs={12} style={{ position: "relative" }}>
+            <img
+              src="https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="
+              className="cover-image"
+              alt="รูปภาพเบียร์"
+            ></img>
+            <Typography variant="h3" className="center-offset">
+              {beer.name}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className="p-16">
+              <Typography variant="h5">รายละเอียด</Typography>
+              <Paper
+                className="p-16"
+                elevation={4}
+                style={{ minHeight: "50px" }}
+              >
+                <Typography variant="subtitle2">{beer.description}</Typography>
+              </Paper>
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className="p-16">
+              <Typography variant="h5">ส่วนผสม</Typography>
+              <Stack spacing={1}>
+                {beer.ingredients.map((ingredient) => (
+                  <Box style={{ display: "flex", width: "100%" }}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={8}>
+                        <Paper className="p-16" elevation={4}>
+                          <Typography variant="subtitle2">
+                            {ingredient.name}
+                          </Typography>
                         </Paper>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Paper className="p-16">
-                            <Typography variant="h5">
-                                ส่วนผสม
-                            </Typography>
-                            <Stack spacing={1}>
-                                {beer.ingredients.map(ingredient => (
-                                    <Box style={{ display: 'flex', width: '100%' }}>
-                                        <Grid container spacing={2}>
-                                            <Grid item xs={8}>
-                                                <Paper className="p-16" elevation={4}>
-                                                    <Typography variant="subtitle2">
-                                                        {ingredient.name}
-                                                    </Typography>
-                                                </Paper>
-                                            </Grid>
-                                            <Grid item xs={2}>
-                                                <Paper className="p-16 ta-center" elevation={4}>
-                                                    <Typography variant="subtitle2">
-                                                        {ingredient.quantity}
-                                                    </Typography>
-                                                </Paper>
-                                            </Grid>
-                                            <Grid item xs={2}>
-                                                <Paper className="p-16 ta-center" elevation={4}>
-                                                    <Typography variant="subtitle2">
-                                                        {ingredient.unit}
-                                                    </Typography>
-                                                </Paper>
-                                            </Grid>
-                                        </Grid>
-                                    </Box>
-
-                                ))}
-                            </Stack>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Paper className="p-16 ta-center" elevation={4}>
+                          <Typography variant="subtitle2">
+                            {ingredient.quantity}
+                          </Typography>
                         </Paper>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Paper className="p-16">
-                            <Typography variant="h5">
-                                ขั้นตอนการทำ
-                            </Typography>
-                            <Stack spacing={1}>
-                                {beer.methods.map((method, index) => (
-                                    <Paper className="p-16" elevation={4}>
-                                        <Typography variant="subtitle2">
-                                            {index + 1}. {method}
-                                        </Typography>
-                                    </Paper>
-                                ))}
-                            </Stack>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Paper className="p-16 ta-center" elevation={4}>
+                          <Typography variant="subtitle2">
+                            {ingredient.unit}
+                          </Typography>
                         </Paper>
+                      </Grid>
                     </Grid>
-                </Grid>
-            </Container>
-        </>
-    )
+                  </Box>
+                ))}
+              </Stack>
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className="p-16">
+              <Typography variant="h5">ขั้นตอนการทำ</Typography>
+              <Stack spacing={1}>
+                {beer.methods.map((method, index) => (
+                  <Paper className="p-16" elevation={4}>
+                    <Typography variant="subtitle2">
+                      {index + 1}. {method}
+                    </Typography>
+                  </Paper>
+                ))}
+              </Stack>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 }
