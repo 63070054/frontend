ac4005a1cf2f8bc8375fe60b513fae2420103592
ntp_vv <63070054@kmitl.ac.th>
2022-12-22 04:07:56 +0700

    fix some SearchBeer and fix tsconfig.json

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -75,7 +75,7 @@ export default function HomeScreen({ isLogin }: IsLoginProp) {
         imageUrl: "https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w=",
     },]);
 
-    let filterBeers = beers.filter(beer => beer.name.includes(queryBeer))
+    let filterBeers = beers.filter(beer => beer.name.includes(queryBeer) || beer.description.includes(queryBeer))
 
     if (selectedIngredients.length > 0) {
         filterBeers = beers.filter(beer => {
